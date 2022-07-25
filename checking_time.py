16f95cd0c44c6cb9bb711ed1c561c5f35e920b0d
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2022-07-25 14:01:18 +0000

    bug fix

--- checking_time.py
+++ checking_time.py
@@ -1,22 +1,18 @@
 import pytz
 import datetime
 
-def check_six():
-  est = pytz.timezone('US/Eastern')
-  now = datetime.datetime.now().astimezone(est)
-
-  current_time = now.strftime("%H")
-  if current_time == "06":
-    return 0
-  else:
-    return 1
-
-def check_day():
+def check_six():  
   est = pytz.timezone('US/Eastern')
   now = datetime.datetime.now().astimezone(est)
   
-  d = now.strftime("%d")
-  m = now.strftime("%m")
-  y = now.strftime("%Y")
-
-  return m, d, y
+  d = int(now.strftime("%d"))
+  m = int(now.strftime("%m"))
+  y = int(now.strftime("%Y"))
+  if datetime.date(y,m,d).weekday() < 5:
+    current_time = now.strftime("%H")
+    if current_time == "06":
+      return 0
+    else:
+      return 1
+  else:
+    return 1
