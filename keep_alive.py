0628deb588ed7fdb4950e91749f7cb71317a588e
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2022-07-25 13:51:28 +0000

    finished https://syep-health-screen.s9shangmin.repl.co/

--- keep_alive.py
+++ keep_alive.py
@@ -11,8 +11,20 @@ def home():
 @app.route('/', methods=['POST'])
 def my_form_post():
   if request.method == "POST":
-    pass
-
+    fname = request.form["first"]
+    lname = request.form["last"]
+    email = request.form["email"]
+    if "database" in db.keys():
+      database = db["database"]
+      data = [fname, lname, email]
+      database.append(data)
+      db["database"] = database
+    else:
+      database = []
+      data = [[fname, lname, email]]
+      db["database"] = data
+    print("new profile added {}, {}".format(fname, lname))
+    return render_template("success.html")
 def run():
   app.run(host='0.0.0.0', port=8080)
 
--- main.py
+++ main.py
@@ -1,2 +1,54 @@
 from keep_alive import keep_alive
+from replit import db
+import asyncio
+from checking_time import check_six
+import algorithm
+
+# this loop will be async
+async def loop():
+  # this segment is to see if theres nothing in database, wait until someone posts something into the database.
+  while True:
+    if "database" in db.keys():
+      database = db["database"]
+      for i in range(len(db["database"])):
+        print(str(i+1), db["database"][i][0], db["database"][i][1])
+      break
+    else:
+      await asyncio.sleep(60)
+
+  # Constant or init variables
+  cool_down = 0
+  
+  while True:
+    if check_six() == 0:
+      # it's 6
+      if cool_down == 0:
+        # init run
+        try:
+          database = db["database"]
+        except Exception as e:
+          print(e)
+        try:
+          algorithm.execute(database)
+        except Exception as e:
+          print(e)
+          # make it go on cooldown
+        cool_down = 1
+
+    if cool_down == 0:
+      # ready
+      await asyncio.sleep(60)
+
+    # if ran already at 6
+    elif cool_down == 1:
+      if check_six() == 1:
+        # this makes sure that the code runs once at 6, and goes into cooldown. Once it's not 6, it goes off cooldown and becomes available again.
+        cool_down = 0
+        # no longer 6
+        await asyncio.sleep(60)
+      else:
+        # not ready
+        await asyncio.sleep(60)
+
 keep_alive()
+asyncio.run(loop())
